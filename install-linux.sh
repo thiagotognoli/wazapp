27c00914d01e8b0600f84e32281217dba9726d46
Thiago Tognoli <thiagotognoli@gmail.com>
2020-03-23 13:35:10 -0300

    build appimage

--- install-linux.sh
+++ install-linux.sh
@@ -5,7 +5,10 @@ currentDirWithSpace=$(echo "$currentDir" | sed 's/ /\\s/g')
 
 cd $currentDir
 npm install
+npm run build
+mv dist/*.AppImage dist/wazapp.AppImage
+chmod a+x dist/wazapp.AppImage
 
 #xprop | grep 'CLASS'
-echo -e '[Desktop Entry]\nName=wazapp\nGenericName=wazapp\nExec='"$currentDirWithSpace"'/run.sh\nTerminal=false\nStartupNotify=false\nType=Application\nIcon='"$currentDirWithSpace"'/assets/icons/icon.png\nStartupWMClass=ws-custom-client\nComment=Unofficial desktop client for WhatsApp Web\nCategories=Network\nTryExec='"$currentDirWithSpace"'/run.sh\nActions=' | tee "$HOME/.local/share/applications/wazapp.desktop"
+echo -e '[Desktop Entry]\nName=wazapp\nGenericName=wazapp\nExec='"$currentDirWithSpace"'/dist/wazapp.AppImage\nTerminal=false\nStartupNotify=false\nType=Application\nIcon='"$currentDirWithSpace"'/assets/icons/icon.png\nStartupWMClass=ws-custom-client\nComment=Unofficial desktop client for WhatsApp Web\nCategories=Network\nTryExec='"$currentDirWithSpace"'/dist/wazapp.AppImage\nActions=' | tee "$HOME/.local/share/applications/wazapp.desktop"
 #/usr/share/applications
